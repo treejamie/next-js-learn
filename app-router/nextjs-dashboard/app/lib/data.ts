1c12d5a6e29b1ef41fa6a9d087a684f78538aa47
treejamie <me@treejamie.com>
2025-04-09 07:47:02 +0100

    There was little code in this one, it was an exercise in speed and sleep

--- app-router/nextjs-dashboard/app/lib/data.ts
+++ app-router/nextjs-dashboard/app/lib/data.ts
@@ -13,16 +13,12 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
+    // this is where chapter 8 inserted a delay.
+    // it was to labour the point that with dynamic rendering
+    // you can only be as fast as your slowest fetch.
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
+    // get the data and return it
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
-
     return data;
   } catch (error) {
     console.error('Database Error:', error);
